efff04c7e5eef7fa338a9b11241fc91686ead75d
VaroujanA <61308674+VaroujanA@users.noreply.github.com>
2020-03-20 12:37:15 +0400

    Update if within radius play music.py

--- if within radius play music.py
+++ if within radius play music.py
@@ -1,10 +1,10 @@
 import json
 from playsound import playsound
 
-Curlatitude = 40.191729
-Curlongitude = 44.521001
+Curlatitude = 
+Curlongitude = 
 
-radius = 0.5 / 85.39
+radius = input_Km / 85.39
 
 with open("Baghramyan_Avn_AUA.json.json") as data_file:
     Destination = json.load(data_file)
