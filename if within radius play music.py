a5dc20a5e5183691f39823b2f4ee37f49da8ceb7
VaroujanA <61308674+VaroujanA@users.noreply.github.com>
2020-05-03 21:16:29 +0400

    Update if within radius play music.py

--- if within radius play music.py
+++ if within radius play music.py
@@ -1,17 +1,33 @@
 import json
 from playsound import playsound
 
-Curlatitude = #Input_from_cordinate_samples
-Curlongitude = #Input_from_cordinate_samples
+Curlatitude = 40.1974
+Curlongitude = 44.5050
+radius = 0.5 / 85.39
 
-radius = Input_Km / 85.39
+def get_des_info():
+    with open("Baghramyan_Avn_AUA.json") as data_file:
+        Destination = json.load(data_file)
 
-with open("Baghramyan_Avn_AUA.json") as data_file:
-    Destination = json.load(data_file)
+    return Destination
 
+def des_to_coordinates(des):
+    latitude = float(des["lat"])
+    longitude = float(des["long"])
 
-latitude = float(Destination["lat"])
-longitude = float(Destination["long"])
+    return latitude, longitude
 
-if ( ( ( (Curlatitude - latitude)**2 ) + ( (Curlongitude - longitude)**2) ) <= radius**2 ):
-    playsound('land on the horizon.mp3')
+def check_if_in_radius(lat, long):
+    if ( ( ( (Curlatitude - lat)**2 ) + ( (Curlongitude - long)**2) ) <= radius**2 ):
+        playsound('land on the horizon.mp3')
+
+def main():
+    my_des = get_des_info()
+
+    coordinate= des_to_coordinates(my_des)
+    lat = coordinate[0]
+    long = (coordinate[1])
+
+    check_if_in_radius(lat,long)
+
+main()
